be28c4a3273262221146cf5e0333330234766503
Leleat <atule@pm.me>
2020-04-05 21:29:48 +0200

    Added separated profiles for scene and script viewports

--- addons/CustomDFM/MenuButton.gd
+++ addons/CustomDFM/MenuButton.gd
@@ -1,3 +1,9 @@
+"""
+The first 3 items in the list are non-dock items (here docks are = all editor docks + bottom panel). Then come the docks for the 2D/3D viewport followed by a separator (non-dock item).
+After that, all docks will be listed again for the Script viewport. That means in total there are 4 non-dock items. So the PopupMenu's structure looks like size (top to bottom): 
+(3 x non-dock items) + (dock_count x dock items) + (non-dock separator) + (dock_count x dock items)
+"""
+
 tool
 extends MenuButton
 
@@ -5,8 +11,15 @@ extends MenuButton
 var BASE_CONTROL_VBOX : VBoxContainer
 var INTERFACE : EditorInterface
 var DFM_BUTTON : ToolButton
-var initial_screen_changed_to_script : bool = true
 var settings_updated : bool = false
+var dock_count : int = 0 # includes bottom panel
+var current_main_screen : String
+var rhsplit_visible : bool
+var leftleft_visible : bool
+var leftright_visible : bool
+var first_change_to_script_view : bool = true
+var dfm_enabled_on_scene : bool = false
+var dfm_enabled_on_script : bool = false
 
 
 func _ready() -> void:
@@ -17,13 +30,13 @@ func _ready() -> void:
 
 
 func _on_PopupMenu_index_pressed(index : int) -> void:
-	get_popup().set_item_checked(index, not get_popup().is_item_checked(index))
+	get_popup().toggle_item_checked(index)
 	settings_updated = true
 
 
 func _on_PopupMenu_hide() -> void:
 	if settings_updated:
-		_save_setting()
+		save_settings()
 		settings_updated = false
 
 
@@ -32,86 +45,144 @@ func _on_MenuButton_pressed() -> void:
 
 
 func _on_DFM_BUTTON_pressed() -> void:
-	_show_docks()
+	match current_main_screen: # for node selection/script opening via SceneTreeDock
+		"2D", "3D": 
+			if DFM_BUTTON.pressed:
+				dfm_enabled_on_scene = true
+			else:
+				dfm_enabled_on_scene = false
+		"Script":
+			if DFM_BUTTON.pressed:
+				dfm_enabled_on_script = true
+			else:
+				dfm_enabled_on_script = false
+	
+	update_dock_visibility()
 
 
 func _on_main_screen_changed(new_screen : String) -> void:
-	yield(get_tree(), "idle_frame")
-	if initial_screen_changed_to_script:
+	current_main_screen = new_screen
+	if first_change_to_script_view and get_popup().get_item_count() > 2: # to autoswitch to DFM when switching to "Script" view the first time, if it is enabled
 		if get_popup().is_item_checked(1):
 			if new_screen == "Script":
 				DFM_BUTTON.emit_signal("pressed")
-				initial_screen_changed_to_script = false
+				first_change_to_script_view = false
 		else:
-			initial_screen_changed_to_script = false
-	_show_docks()
+			first_change_to_script_view = false
+	
+	if not dfm_enabled_on_scene and DFM_BUTTON.pressed and current_main_screen in ["2D", "3D"]: # for node selection via SceneTreeDock
+		DFM_BUTTON.pressed = false
+	if dfm_enabled_on_script and not DFM_BUTTON.pressed and current_main_screen == "Script": # for script opening via SceneTreeDock
+		DFM_BUTTON.pressed = true
+	
+	yield(get_tree(), "idle_frame")
+	update_dock_visibility()
 
 
-func _show_docks(tab : int = -1) -> void: # called via via DFM button pressed and dock tab changed
+func update_dock_visibility(tab : int = -1) -> void: # called via signals on DFM button press or tab change of dock
+	for index in dock_count - 1:
+		var dock = get_dock(get_popup().get_item_text(index + 3))
+		dock[0].get_parent().set_tab_disabled(dock[0].get_index(), false)
+	
 	if DFM_BUTTON.pressed:
-		var vis_tabcontainer : Array
+		var visible_tabcontainer : Array
+		rhsplit_visible = false
+		leftleft_visible = false
+		leftright_visible = false
+		
 		# show tabs
-		for index in get_popup().get_item_count() - 4: # - 4 and index + 3 because non-dock items are in the list
-			var dock = _get_dock(get_popup().get_item_text(index + 3))
-			if get_popup().is_item_checked(index + 3):
-				dock.get_parent().show()
-				dock.get_parent().get_parent().show()
-				dock.get_parent().get_parent().get_parent().show()
-				if not dock.get_parent() in vis_tabcontainer:
-					vis_tabcontainer.push_back(dock.get_parent())
+		for index in dock_count - 1: 
+			var idx = 3 + index  if current_main_screen in ["2D", "3D"] else 3 + index + dock_count + 1
+			var dock = get_dock(get_popup().get_item_text(idx))
+			if get_popup().is_item_checked(idx):
+				
+				if dock[1].begins_with("Right"):
+					rhsplit_visible = true
+				elif dock[1] == "Left left":
+					leftleft_visible = true
+				elif dock[1] == "Left right":
+					leftright_visible = true
+				
+				dock[0].get_parent().show()
+				dock[0].get_parent().get_parent().show()
+				if not dock[0].get_parent() in visible_tabcontainer:
+					visible_tabcontainer.push_back(dock[0].get_parent())
 			else:
-				dock.get_parent().set_tab_disabled(dock.get_index(), true)
+				dock[0].get_parent().set_tab_disabled(dock[0].get_index(), true)
+		
+		if rhsplit_visible:
+			BASE_CONTROL_VBOX.get_child(1).get_child(1).get_child(1).get_child(1).show()
+		else:
+			BASE_CONTROL_VBOX.get_child(1).get_child(1).get_child(1).get_child(1).hide()
+		if leftleft_visible:
+			BASE_CONTROL_VBOX.get_child(1).get_child(0).show()
+		else:
+			BASE_CONTROL_VBOX.get_child(1).get_child(0).hide()
+		if leftright_visible:
+			BASE_CONTROL_VBOX.get_child(1).get_child(1).get_child(0).show()
+		else:
+			BASE_CONTROL_VBOX.get_child(1).get_child(1).get_child(0).hide()
+		
 		# change tab to active one
-		for tabcontainer in vis_tabcontainer:
+		for tabcontainer in visible_tabcontainer:
 			if tabcontainer.get_tab_disabled(tabcontainer.current_tab):
 				for idx in tabcontainer.get_tab_count():
 					if not tabcontainer.get_tab_disabled(idx):
 						tabcontainer.current_tab = idx
 						break
+		
 		# bottom panel
-		if get_popup().is_item_checked(get_popup().get_item_count() - 1):
+		var idx = get_popup().get_item_count() - 1 if current_main_screen == "Script" else 3 + dock_count - 1
+		if get_popup().is_item_checked(idx):
 			BASE_CONTROL_VBOX.get_child(1).get_child(1).get_child(1).get_child(0).get_child(0).get_child(1).show()
 	
 	else:
-		for index in get_popup().get_item_count() - 4: # - 4 and index + 3 because non-dock items are in the list
-			var dock = _get_dock(get_popup().get_item_text(index + 3))
-			dock.get_parent().set_tab_disabled(dock.get_index(), false)
+		# for node selection via SceneTreeDock
+		for index in dock_count - 1: 
+			var dock = get_dock(get_popup().get_item_text(index + 3))
+			dock[0].get_parent().show()
+			dock[0].get_parent().get_parent().show()
+			dock[0].get_parent().get_parent().get_parent().show() 
 
 
-func _get_dock(dclass : String) -> Node: # dclass : "FileSystemDock" || "ImportDock" || "NodeDock" || "SceneTreeDock" || "InspectorDock" are defaults
+func get_dock(dclass : String): # dclass : "FileSystemDock" || "ImportDock" || "NodeDock" || "SceneTreeDock" || "InspectorDock" are defaults
 	for tabcontainer in BASE_CONTROL_VBOX.get_child(1).get_child(0).get_children(): # LEFT left
 		for dock in tabcontainer.get_children():
 			if dock.get_class() == dclass or dock.name == dclass:
-				return dock
+				return [dock, "Left left"]
 	for tabcontainer in BASE_CONTROL_VBOX.get_child(1).get_child(1).get_child(0).get_children(): # LEFT right
 		for dock in tabcontainer.get_children():
 			if dock.get_class() == dclass or dock.name == dclass:
-				return dock
+				return [dock, "Left right"]
 	for tabcontainer in BASE_CONTROL_VBOX.get_child(1).get_child(1).get_child(1).get_child(1).get_child(0).get_children(): # RIGHT left
 		for dock in tabcontainer.get_children():
 			if dock.get_class() == dclass or dock.name == dclass:
-				return dock
+				return [dock, "Right left"]
 	for tabcontainer in BASE_CONTROL_VBOX.get_child(1).get_child(1).get_child(1).get_child(1).get_child(1).get_children(): # RIGHT right
 		for dock in tabcontainer.get_children():
 			if dock.get_class() == dclass or dock.name == dclass:
-				return dock
-	
+				return [dock, "Right right"]
 	return null
 
 
-func _save_setting() -> void:
+func save_settings() -> void:
 	var config = ConfigFile.new()
-	for index in get_popup().get_item_count():
-		config.set_value(get_popup().get_item_text(index), "checked", get_popup().is_item_checked(index) if get_popup().is_item_checked(index) else "")
+	config.set_value("Usage", get_popup().get_item_text(0).replace(" ", "_"), "true" if get_popup().is_item_checked(0) else "")
+	config.set_value("Usage", get_popup().get_item_text(1).replace(" ", "_"), "true" if get_popup().is_item_checked(1) else "")
+	for index in dock_count: 
+		# scene editor
+		config.set_value("Scene_Editor", get_popup().get_item_text(3 + index).replace(" ", "_"), "true" if get_popup().is_item_checked(3 + index) else "")
+		# script editor
+		config.set_value("Script_Editor", get_popup().get_item_text(3 + index + dock_count + 1).replace(" ", "_"), "true" if get_popup().is_item_checked(3 + index + dock_count + 1) else "")
 	config.save("user://custom_dfm_settings.cfg")
 
 
 func load_settings() -> void:
 	get_popup().clear()
 	get_popup().rect_size = Vector2(1, 1)
-	get_popup().add_check_item("Use DFM on editor start")
-	get_popup().add_check_item("Use DFM in \"Script\" screen on editor start")
-	get_popup().add_separator("  Options  ")
+	get_popup().add_check_item("Use DFM in scene viewport on editor start")
+	get_popup().add_check_item("Use DFM in script viewport on editor start")
+	get_popup().add_separator("  2D/3D Settings  ")
 	
 	for tabcontainer in BASE_CONTROL_VBOX.get_child(1).get_child(0).get_children(): # LEFT left
 		for dock in tabcontainer.get_children():
@@ -125,11 +196,19 @@ func load_settings() -> void:
 	for tabcontainer in BASE_CONTROL_VBOX.get_child(1).get_child(1).get_child(1).get_child(1).get_child(1).get_children(): # RIGHT right
 		for dock in tabcontainer.get_children():
 			get_popup().add_check_item(dock.get_class() if dock.get_class().findn("Dock") != -1 else dock.name)
+	get_popup().add_check_item("Bottom Panel") 
 	
-	get_popup().add_check_item("Bottom Panel") # Bottom panel needs to be the last one
+	get_popup().add_separator("  Script Settings  ")
+	for index in get_popup().get_item_count() - 4: 
+		get_popup().add_check_item(get_popup().get_item_text(index + 3))
+	
+	dock_count = (get_popup().get_item_count() - 4) / 2
 	
 	var config = ConfigFile.new()
 	var error = config.load("user://custom_dfm_settings.cfg")
 	if error == OK:
-		for index in get_popup().get_item_count():
-			get_popup().set_item_checked(index, config.get_value(get_popup().get_item_text(index), "checked", false) as bool)
+		get_popup().set_item_checked(0, config.get_value("Usage", get_popup().get_item_text(0).replace(" ", "_"), false) as bool)
+		get_popup().set_item_checked(1, config.get_value("Usage", get_popup().get_item_text(1).replace(" ", "_"), false) as bool)
+		for index in dock_count:
+			get_popup().set_item_checked(3 + index, config.get_value("Scene_Editor", get_popup().get_item_text(index + 3).replace(" ", "_"), false) as bool)
+			get_popup().set_item_checked(3 + index + dock_count + 1, config.get_value("Script_Editor", get_popup().get_item_text(index + 3).replace(" ", "_"), false) as bool)
--- addons/CustomDFM/plugin.gd
+++ addons/CustomDFM/plugin.gd
@@ -2,47 +2,45 @@ tool
 extends EditorPlugin
 
 
-var custom_dfm_button : MenuButton = load("res://addons/CustomDFM/MenuButton.tscn").instance()
+var plugin_scene : MenuButton = load("res://addons/CustomDFM/MenuButton.tscn").instance()
+var dfm_button = get_editor_interface().get_base_control().get_child(1).get_child(1).get_child(1).get_child(1).get_child(0).get_child(0).get_child(0).get_child(0)\
+			.get_child(0).get_child(get_editor_interface().get_base_control().get_child(1).get_child(1).get_child(1).get_child(1).\
+			get_child(0).get_child(0).get_child(0).get_child(0).get_child(0).get_child_count() - 1)
 
 
 func _enter_tree():
-	add_control_to_container(EditorPlugin.CONTAINER_TOOLBAR, custom_dfm_button)
+	add_control_to_container(EditorPlugin.CONTAINER_TOOLBAR, plugin_scene)
 	var BASE_CONTROL_VBOX = get_editor_interface().get_base_control().get_child(1)
-	var DFM_BUTTON = get_editor_interface().get_base_control().get_child(1).get_child(1).get_child(1).get_child(1).get_child(0).get_child(0).get_child(0).get_child(0)\
-			.get_child(0).get_child(get_editor_interface().get_base_control().get_child(1).get_child(1).get_child(1).get_child(1).\
-			get_child(0).get_child(0).get_child(0).get_child(0).get_child(0).get_child_count() - 1)
 	
-	connect("main_screen_changed", custom_dfm_button, "_on_main_screen_changed")
-	DFM_BUTTON.connect("pressed", custom_dfm_button, "_on_DFM_BUTTON_pressed")
-	custom_dfm_button.BASE_CONTROL_VBOX = BASE_CONTROL_VBOX
-	custom_dfm_button.DFM_BUTTON = DFM_BUTTON
-	custom_dfm_button.INTERFACE = get_editor_interface()
+	connect("main_screen_changed", plugin_scene, "_on_main_screen_changed")
+	dfm_button.connect("pressed", plugin_scene, "_on_DFM_BUTTON_pressed")
+	plugin_scene.BASE_CONTROL_VBOX = BASE_CONTROL_VBOX
+	plugin_scene.DFM_BUTTON = dfm_button
+	plugin_scene.INTERFACE = get_editor_interface()
 	get_editor_interface().get_editor_settings().set_setting("interface/editor/separate_distraction_mode", true)
 	
 	for tabcontainer in BASE_CONTROL_VBOX.get_child(1).get_child(0).get_children(): # LEFT left
-		if not tabcontainer.is_connected("tab_changed", custom_dfm_button, "_show_docks"):
-			tabcontainer.connect("tab_changed", custom_dfm_button, "_show_docks")
+		if not tabcontainer.is_connected("tab_changed", plugin_scene, "update_dock_visibility"):
+			tabcontainer.connect("tab_changed", plugin_scene, "update_dock_visibility")
 	for tabcontainer in BASE_CONTROL_VBOX.get_child(1).get_child(1).get_child(0).get_children(): # LEFT right
-		if not tabcontainer.is_connected("tab_changed", custom_dfm_button, "_show_docks"):
-			tabcontainer.connect("tab_changed", custom_dfm_button, "_show_docks")
+		if not tabcontainer.is_connected("tab_changed", plugin_scene, "update_dock_visibility"):
+			tabcontainer.connect("tab_changed", plugin_scene, "update_dock_visibility")
 	for tabcontainer in BASE_CONTROL_VBOX.get_child(1).get_child(1).get_child(1).get_child(1).get_child(0).get_children(): # RIGHT left
-		if not tabcontainer.is_connected("tab_changed", custom_dfm_button, "_show_docks"):
-			tabcontainer.connect("tab_changed", custom_dfm_button, "_show_docks")
+		if not tabcontainer.is_connected("tab_changed", plugin_scene, "update_dock_visibility"):
+			tabcontainer.connect("tab_changed", plugin_scene, "update_dock_visibility")
 	for tabcontainer in BASE_CONTROL_VBOX.get_child(1).get_child(1).get_child(1).get_child(1).get_child(1).get_children(): # RIGHT right
-		if not tabcontainer.is_connected("tab_changed", custom_dfm_button, "_show_docks"):
-			tabcontainer.connect("tab_changed", custom_dfm_button, "_show_docks")
+		if not tabcontainer.is_connected("tab_changed", plugin_scene, "update_dock_visibility"):
+			tabcontainer.connect("tab_changed", plugin_scene, "update_dock_visibility")
 
 
 func _ready() -> void:
 	yield(get_tree(), "idle_frame")
-	custom_dfm_button.load_settings()
+	plugin_scene.load_settings()
 	yield(get_tree(), "idle_frame")
-	if custom_dfm_button.get_popup().is_item_checked(0):
-		get_editor_interface().get_base_control().get_child(1).get_child(1).get_child(1).get_child(1).get_child(0).get_child(0).get_child(0).get_child(0)\
-				.get_child(0).get_child(get_editor_interface().get_base_control().get_child(1).get_child(1).get_child(1).get_child(1).\
-				get_child(0).get_child(0).get_child(0).get_child(0).get_child(0).get_child_count() - 1).emit_signal("pressed")
+	if plugin_scene.get_popup().is_item_checked(0):
+		dfm_button.emit_signal("pressed")
 
 
 func _exit_tree():
-	remove_control_from_container(EditorPlugin.CONTAINER_TOOLBAR, custom_dfm_button)
-	custom_dfm_button.queue_free()
+	remove_control_from_container(EditorPlugin.CONTAINER_TOOLBAR, plugin_scene)
+	plugin_scene.queue_free()
